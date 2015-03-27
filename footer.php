429eae3e2b1122e4c6f976f415d396c94e56052f
Alex Torrance <alex_tea@Gerty.local>
2015-03-27 10:35:33 +0000

    Split template into wordpress includes

---
+++ footer.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains footer content and the closing of the #main and #page div elements.
+ *
+ * @package WordPress
+ * @subpackage Twenty_Fourteen
+ * @since Twenty Fourteen 1.0
+ */
+?>
+    </main>
+
+    <footer id="footer">
+      <div class="footer-wrapper">
+        <div class="grid-row">
+          <div class="column-two-thirds">
+            <ul class="footer-links">
+              <li><a href="<?php echo esc_url( home_url( '/' ) ); ?>contact">Contact</a></li>
+              <li><a href="<?php echo esc_url( home_url( '/' ) ); ?>about">About</a></li>
+              <li><a href="<?php echo esc_url( home_url( '/' ) ); ?>terms">Terms</a></li>
+            </ul>
+          </div>
+        </div>
+        
+        <div class="text">
+          <p class="acknowledgement">The Youth Justice Legal Centre is brought to you by <a href="#">Just for Kids Law</a>. Thanks to our funders Esmée Fairbairn Foundation, Mark Leonard Trust, Legal Education Foundation, Network for Social Change.</p>
+
+          <p>The content of this website represents the interpretation of the law in England and Wales by the Youth Justice Legal Centre. YJLC aims to be legally accurate, it should not replace independent legal advice. YJLC cannot take responsibility for the accuracy of external websites.</p>
+
+          <p>Youth Justice Legal Centre, 180 North Gower Street, London NW1 2NB</p>
+          
+          <p>&copy;Youth Justice Legal Centre 2015. All rights reserved.</p>
+        </div>
+      </div>
+    </footer>
+  </div>
+
+
+    <script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/javascripts/cookies.js"></script>
+    <script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/javascripts/cookie-bar.js"></script>
+
+  <?php wp_footer(); ?>
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,78 @@
+<?php
+/**
+ * The Header for our theme
+ *
+ * Displays all of the <head> section and everything up till <div id="main">
+ *
+ * @package YJLC Template
+ */
+?><!DOCTYPE html>
+<!--[if lt IE 9]><html class="lte-ie8" <?php language_attributes(); ?>><![endif]-->
+<!--[if gt IE 8]><!--><html <?php language_attributes(); ?>><!--<![endif]-->
+<head>
+  <meta charset="<?php bloginfo( 'charset' ); ?>">
+  <title><?php wp_title( '|', true, 'right' ); ?></title>
+  <link rel="profile" href="http://gmpg.org/xfn/11">
+  <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+    
+  <link href='http://fonts.googleapis.com/css?family=Lato:300,400,700,400italic' rel='stylesheet' type='text/css'>
+
+  <!--[if gt IE 8]>
+  <!--><link href="<?php echo get_template_directory_uri(); ?>/style.css" media="screen" rel="stylesheet" type="text/css" /><!--<![endif]-->
+  <!--[if lte IE 8]>
+  <link href="<?php echo get_template_directory_uri(); ?>/ie.css" media="screen" rel="stylesheet" type="text/css" />
+  <![endif]-->
+
+  <link href="<?php echo get_template_directory_uri(); ?>/print.css" media="print" rel="stylesheet" type="text/css" />
+
+  <!--[if lt IE 9]>
+  <script src="<?php echo get_template_directory_uri(); ?>/javscripts/html5.js"></script>
+  <![endif] -->
+
+  <link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
+
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+    
+  <?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?>>
+  <script type="text/javascript">document.body.className = ((document.body.className) ? document.body.className + ' js-enabled' : 'js-enabled');</script>
+
+  <div id="skiplink-container">
+    <div>
+      <a href="#content" class="skiplink">Skip to main content</a>
+    </div>
+  </div>
+
+  <div id="global-cookie-message">
+    <p>YJLC uses cookies to make the site simpler. <a href="<?php echo esc_url( home_url( '/' ) ); ?>cookies">Find out more about cookies</a></p>
+  </div>
+
+  <header role="banner" id="global-header">
+    <div class="header-wrapper">
+      <div class="header-global">
+        <div class="header-logo">
+          <a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
+            <img src="<?php echo get_template_directory_uri(); ?>/images/yjlc-logo-white-2x.png" alt="YJLC logo">
+            <span class="visuallyhidden">Youth Justice Legal Centre</span>
+          </a>
+        </div>
+
+        <?php 
+          wp_nav_menu( array( 
+            'theme_location' => 'main_menu', 
+            'menu_class' => 'main-menu' )
+           );
+        ?>
+
+      </div>
+    </div>
+  </header>
+
+  <div id="feedback">
+    <p>This is a new website. Your <a href="<?php echo esc_url( home_url( '/' ) ); ?>feedback">feedback</a> will help us to improve it.</p>
+  </div>
+
+  <div id="wrapper">
+    <main id="content">
